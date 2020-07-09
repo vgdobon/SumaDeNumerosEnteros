17fb44830049faff7331952d1c89af4d8954c0d0
Victor <vgdobon91@gmail.com>
2020-07-09 10:16:10 -0400

    Suma de numeros enteros positivos/negativos.Divisibles

---
+++ src/DivisoresDeUnNumero.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DivisoresDeUnNumero {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Dime de que número quieres saber sus divisores");
+        int numero=input.nextInt();
+
+        while(numero<0){
+            System.out.println("No estas introduciendo un numero positivo");
+            System.out.println("Dime de que número quieres saber sus divisores");
+            numero=input.nextInt();
+        }
+        System.out.print("Los divisibles de "+numero+" son:");
+
+        for(int i=1;i<=numero;i++){
+            if(numero%i==0){
+                System.out.print(i+" ");
+            }
+        }
+
+
+    }
+}
