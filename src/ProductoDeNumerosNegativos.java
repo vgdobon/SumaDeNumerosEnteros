e58791f0ee7c5ff85dd1fb6157a82a5b2ae8a5ed
Victor <vgdobon91@gmail.com>
2020-07-09 09:54:54 -0400

    Suma de numeros enteros positivos/negativos

--- src/ProductoDeNumerosNegativos.java
+++ src/ProductoDeNumerosNegativos.java
@@ -5,25 +5,30 @@ public class ProductoDeNumerosNegativos {
         Scanner input=new Scanner(System.in);
 
         boolean error=true;
-        boolean resultadoPositivo=false;
         boolean resultadoNegativo=false;
         int suma=0;
-        int numeroAbsoluto=Math.abs();
+
 
         System.out.println("Dame un numero negativo/positivo");
         int numero=input.nextInt();
         System.out.println("Dame otro numero negativo/positivo");
         int numero2=input.nextInt();
+        int numeroAbsoluto=Math.abs(numero);
+        int numero2Absoluto=Math.abs(numero2);
 
-        if((numero>=0&&numero>=0)||(numero<=0&&numero2<=0)){
-                resultadoPositivo=true;
-        }else{
-            resultadoNegativo=true;
+        if((numero<0&&numero>0)||(numero>0&&numero2<0)){
+                resultadoNegativo=true;
         }
 
+        for(int i=0;i<numeroAbsoluto;i++){
+            suma=suma+numero2Absoluto;
+        }
 
+        if(resultadoNegativo){
+            suma=suma*-1;
+        }
 
-
+        System.out.println("El resultado de "+numero+"*"+numero2+"="+suma);
 
     }
 }
